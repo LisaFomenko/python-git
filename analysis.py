59cc5c4403da0a65215afa99e723e49ef58e5d86
LisaFomenko <liza.15.krivorychko@gmail.com>
2026-06-06 12:51:07 +0300

    Revert "add average sales by city"

--- analysis.py
+++ analysis.py
@@ -1,10 +1,3 @@
-
-feature/average-sales
-
-def average_sales_by_city(df):
-    return df.groupby("city")["sales"].mean()
-  
-def average_sales_by_city(df):
+def total_sales_by_city(df):
     return df.groupby("city")["sales"].sum().sort_values(ascending=False)
 
-main
